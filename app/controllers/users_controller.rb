a5daee87313c811c94c597d3990587089455ed9e
Suyog Nerkar <suyog.nerkar@weboniselab.com>
2014-01-10 14:21:04 +0530

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,15 @@ class UsersController < ApplicationController
   end
 
 
-def show
+  def show
     @user = User.find(params[:id])
   end
-
-
 
 
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -25,9 +24,9 @@ def show
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+     :password_confirmation)
+  end
 
 end
